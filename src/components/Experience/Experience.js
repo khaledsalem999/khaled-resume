d8e9d63ff6fd92845b177a0fb9210b08735fbbdc
khaledsalem999 <khaledsalem999@gmail.com>
2021-10-30 01:33:20 +0200

    jobs section done-ish

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,40 +1,56 @@
 import './Experience.scss'
+import {useState} from "react";
 const experienceData = require('../../data/WorkExperience.json');
 
 const Experience = () => {
 
+    const [currentJobNum, setCurrentJobNum ] = useState(0);
+
     const jobs = experienceData.WorkExperience;
+    const jobNum = experienceData.WorkExperience.length;
+
+    const jobNumIncrease = () => {
+        if(currentJobNum < jobNum-1){
+            setCurrentJobNum(currentJobNum+1);
+        }
+    }
+
+    const jobNumDecrease = () => {
+        if(currentJobNum > 0){
+            setCurrentJobNum(currentJobNum-1);
+        }
+    }
 
     return (
         <div className={'started-content mb-4'}>
             <div className={'d-flex flex-column mb-4'}>
                 <span className={'h-title mb-2'}>Experience</span>
             </div>
-            <div>
-                <div className={'d-flex flex-column mb-4'}>
-                    <span className={'s-title'}>{jobs[0].CompanyName}</span>
-                    <span className={'entity-text mt-2'}>{jobs[0].From} - {jobs[0].To}</span>
-                </div>
-                <div className={'h-text mb-5'}>
-                    <span>{jobs[0].AboutCompany}</span>
-                </div>
-                <div className={'grid-container'}>
-                    {jobs[0].ProjectsList.map((project) =>
-                        <div className={'d-flex flex-row grid-item'}>
+            <button onClick={jobNumIncrease}>increase</button>
+            <button onClick={jobNumDecrease}>Decrease</button>
+            <div className={'d-flex flex-column mb-4 jobs-transition'}>
+                <span className={'s-title'}>{jobs[currentJobNum].CompanyName}</span>
+                <span className={'entity-text mt-2'}>{jobs[currentJobNum].From} - {jobs[currentJobNum].To}</span>
+            </div>
+            <div className={'h-text mb-5'}>
+                <span>{jobs[currentJobNum].AboutCompany}</span>
+            </div>
+            <div className={'grid-container'}>
+                {jobs[currentJobNum].ProjectsList.map((project) =>
+                    <div className={'d-flex flex-row grid-item'}>
+                        <div>
+                            <span className={'entity-text'}>{project.ProjectName}</span>
                             <div>
-                                <span className={'entity-text'}>{project.ProjectName}</span>
-                                <div>
-                                    <ul>
-                                        {project.Tasks.map((task) =>
-                                            <li>{task}</li>
-                                        )}
-                                    </ul>
-                                </div>
+                                <ul>
+                                    {project.Tasks.map((task) =>
+                                        <li>{task}</li>
+                                    )}
+                                </ul>
                             </div>
                         </div>
-                    )
-                    }
-                </div>
+                    </div>
+                )
+                }
             </div>
         </div>
     )
