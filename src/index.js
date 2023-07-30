f26f0b0338c46fdfb68c24455e85043189c8de7e
khaled.salem <khaled.salem.business@gmail.com>
2023-07-30 23:38:20 +0300

    updated some older content

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
